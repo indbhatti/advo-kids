d33b3798e93b8cb1322296ce965d69bb43a7453e
walruii <walruiix@gmail.com>
2024-08-07 21:46:42 +0530

    fixed sessiontype Error

--- app/quiz/[storyline]/page.tsx
+++ app/quiz/[storyline]/page.tsx
@@ -1,31 +1,27 @@
-import QuizLogic from './quizLogic'
-import { getUserById } from '@/server-actions/serveractions';
-import { SessionType, options } from '@/app/api/auth/[...nextauth]/options';
-import { getServerSession } from 'next-auth';
-import { SimpleUser } from '@/models/user';
+import QuizLogic from "./quizLogic";
+import { getUserById } from "@/server-actions/serveractions";
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { Session, getServerSession } from "next-auth";
+import { SimpleUser } from "@/models/user";
 
-
-export default async function Quiz({ params }: { params: { storyline: string } }) {
+export default async function Quiz({
+  params,
+}: {
+  params: { storyline: string };
+}) {
   const storylineNumber = Number(params.storyline);
 
-  const data: SessionType | null = await getServerSession(options);
+  const data: Session | null = await getServerSession(options);
   if (data?.user?.userId) {
     const user: SimpleUser | null = await getUserById(data.user.userId);
     if (user) {
       return (
         <div className="container my-10">
-          <QuizLogic
-            storylineNumber={storylineNumber}
-            user={user}
-          />
+          <QuizLogic storylineNumber={storylineNumber} user={user} />
         </div>
-      )
+      );
     }
   } else {
-    return (
-      <div>
-        Login In
-      </div>
-    )
+    return <div>Login In</div>;
   }
 }
--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,36 +1,52 @@
-import Card from './card'
-import { SessionType, options } from '../api/auth/[...nextauth]/options'
-import { getServerSession } from 'next-auth/next'
-import { SimpleStoryline } from "../../models/storylines"
-import { SimpleUser } from '@/models/user'
-import { getStorylines, getUserById } from '@/server-actions/serveractions'
+import Card from "./card";
+import { options } from "../api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+import { SimpleStoryline } from "../../models/storylines";
+import { SimpleUser } from "@/models/user";
+import { getStorylines, getUserById } from "@/server-actions/serveractions";
+import { Session } from "next-auth";
 
 export default async function play() {
-  const data: SessionType | null = await getServerSession(options)
-  let user: SimpleUser | null = null
+  const data: Session | null = await getServerSession(options);
+  let user: SimpleUser | null = null;
   if (data) {
-    user = await getUserById(data.user.userId)
+    user = await getUserById(data.user.userId);
   }
   if (user) {
-    var storylines: Array<SimpleStoryline> | null = await getStorylines(user.language)
+    var storylines: Array<SimpleStoryline> | null = await getStorylines(
+      user.language,
+    );
   } else {
-    var storylines: Array<SimpleStoryline> | null = await getStorylines("English")
+    var storylines: Array<SimpleStoryline> | null =
+      await getStorylines("English");
   }
 
   if (storylines) {
     return (
       <div>
-        {storylines.map(({ storyline_number, description, title, questions }: { storyline_number: number, description: string, title: string, questions: number }) => (
-          <Card
-            key={storyline_number}
-            storylineNumber={storyline_number}
-            title={title}
-            description={description}
-            questions={questions}
-            user={user}
-          />
-        ))}
+        {storylines.map(
+          ({
+            storyline_number,
+            description,
+            title,
+            questions,
+          }: {
+            storyline_number: number;
+            description: string;
+            title: string;
+            questions: number;
+          }) => (
+            <Card
+              key={storyline_number}
+              storylineNumber={storyline_number}
+              title={title}
+              description={description}
+              questions={questions}
+              user={user}
+            />
+          ),
+        )}
       </div>
-    )
+    );
   }
 }
