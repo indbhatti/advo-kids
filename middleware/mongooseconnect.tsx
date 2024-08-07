adfb7901bd92e80c3c76fa7d337ecde23bfcb302
walruii <walruiix@gmail.com>
2024-08-07 21:24:35 +0530

    formatting

--- middleware/mongooseconnect.tsx
+++ middleware/mongooseconnect.tsx
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export default async function connectMongoDB () {
+export default async function connectMongoDB() {
   if (mongoose.connection.readyState === 1) {
     return mongoose.connection.asPromise();
   }
-  return await mongoose.connect(process.env.MONGO_URI as string)
+  return await mongoose.connect(process.env.MONGO_URI as string);
 }
